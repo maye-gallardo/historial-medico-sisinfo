5c9abf7793c2b374446be0400e612ce6bbd10956
MayeGer <maye.gallardoger@gmail.com>
2018-05-30 08:06:15 -0900

    subiendo heroku

--- src/main/java/com/ucbcba/proyecto/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ucbcba.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import javax.transaction.Transactional;
-
-@Transactional
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
